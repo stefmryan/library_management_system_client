26c18eed3d45344e89689819195edb61d96b2547
stefmryan <stefmryan@gmail.com>
2021-12-26 12:09:38 -0600

    fixing up dev env

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,22 +1,18 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-
-    // const handleBtn = () =>{
-    //     console.log("Clicked btn");
-    // }
-    return(
+  // const handleBtn = () =>{
+  //     console.log("Clicked btn");
+  // }
+  return (
     <div>
-        <div>
-        This is the LandingPage
-        </div>
-        <div>
+      <div>This is the LandingPage</div>
+      <div>
         <Link to="/register">Click Me</Link>
-        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default LandingPage;
