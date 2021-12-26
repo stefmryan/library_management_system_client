04db9bd77e78885ab8cebc90ccad182252e01b52
stefmryan <stefmryan@gmail.com>
2021-12-26 13:02:04 -0600

    adding function handlers to input and select in library account form

--- src/components/LibraryAccountForm/LibraryAccountForm.js
+++ src/components/LibraryAccountForm/LibraryAccountForm.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React /*{ useState }*/ from "react";
+import constants from "../utilities/constants";
 
 const LibraryAccountForm = () => {
+  /*const [accountObj, setAccountObj] = useState({
+    libraryAccountNumber: 0,
+    email: "",
+    handle: "",
+    firstName: "",
+    lastName: "",
+    driverLicenseNumber: "",
+    birthdate: "",
+    telephone: "",
+    street: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    county: {
+      label: "",
+    },
+    accountType: {
+      label: "",
+    },
+  });*/
+
+  const handleInput = () => {
+    console.log("This is input");
+  };
+
+  const handleSelect = () => {
+    console.log("This is select");
+  };
   return (
     <div>
       <form>
         <label>
           First Name
-          <input type="text" />
+          <input type="text" onChange={handleInput} />
         </label>
         <label>
           {" "}
           Last Name
-          <input type="text" />
+          <input type="text" onChange={handleInput} />
         </label>
         <label>
           {" "}
           Street Address
-          <input type="text" />
+          <input type="text" onChange={handleInput} />
         </label>
         <label>
           Street Address
-          <input type="text" />
+          <input type="text" onChange={handleInput} />
         </label>
         <label>
           {" "}
           City
-          <input type="text" />
+          <input type="text" onChange={handleInput} />
         </label>
         <label>
-          <select>
-            <option>IN</option>
+          <select onChange={handleSelect}>
+            {constants.STATE_LIST.map((state, index) => (
+              <option key={index} value={state}>
+                {state}
+              </option>
+            ))}
           </select>
         </label>
         <label>
           Zip Code
-          <input type="text" />
+          <input type="text" onChange={handleInput} />
         </label>
         <label>
           Account Number
