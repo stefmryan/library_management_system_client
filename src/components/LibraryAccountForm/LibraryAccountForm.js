3a655f7aeb03d117e926f7b48c96e65573bcbdb9
stefmryan <stefmryan@gmail.com>
2021-12-26 11:38:12 -0600

    add inputs for library form

--- src/components/LibraryAccountForm/LibraryAccountForm.js
+++ src/components/LibraryAccountForm/LibraryAccountForm.js
@@ -3,7 +3,59 @@ import React from 'react';
 const LibraryAccountForm = () => {
     return(
         <div>
-            This is library account form component
+            <form>
+            <label>First Name
+           <input type="text"/> 
+           </label>
+           <label> Last Name
+           <input type="text"/> 
+           </label>
+           <label> Street Address
+           <input type="text"/> 
+           </label>
+           <label>Street Address
+           <input type="text"/> 
+           </label>
+           <label> City
+           <input type="text"/> 
+           </label>
+           <label>
+           <select>
+               <option>IN</option>
+           </select>
+           </label>
+           <label>Zip Code
+           <input type="text"/> 
+           </label>
+           <label>Account Number
+           <input type="text"/> 
+           </label>
+           <label>Driver License Number
+               <input type="text" />
+           </label>
+           <label>County
+               <select >
+                   <option>Red</option>
+                   <option>Orange</option>
+               </select>
+           </label>
+           <label>Telephone Number
+               <input type="text" />
+           </label>
+           <label>Email
+               <input type="text" />
+           </label>
+           <label>Handle
+               <input type="text" />
+           </label>
+           <label>Account type
+               <select>
+                   <option>Adult</option>
+                   <option>Juvenile</option>
+                   <option>Teacher</option>
+               </select>
+           </label>
+           </form>
         </div>
     )
 }
