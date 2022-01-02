92aa536cd01063c2e67f981138f9058aeae51d71
stefmryan <stefmryan@gmail.com>
2022-01-02 15:18:51 -0600

    fixing padding for components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage/LandingPage";
 import LibraryAccountForm from "./components/LibraryAccountForm/LibraryAccountForm";
 import Navigation from "./components/Navigation/Navigation";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div>
-      <Navigation />
+    <div className={styles.container}>
       <Router>
+        <Navigation />
+
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
           <Route exact path="/register" element={<LibraryAccountForm />} />
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "../Navigation/Navigation.module.css";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div className={styles.container}>
       <div>Checkout</div>
       <div>Checkin</div>
-      <div>Create a New Account</div>
+      <div>
+        <Link to="/register">Create a New Account</Link>
+      </div>
       <div>Find an Item</div>
       <div>Logout</div>
     </div>
