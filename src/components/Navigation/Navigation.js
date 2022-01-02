ae9655e40a73794cf99b850d52ad08f476eabb40
stefmryan <stefmryan@gmail.com>
2022-01-02 15:59:17 -0600

    styling nav bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,16 +6,24 @@ const Navigation = () => {
   return (
     <div className={styles.container}>
       <div>
-        <Link to="/checkout">Checkout</Link>
+        <Link to="/checkout" className={styles.link}>
+          Checkout
+        </Link>
       </div>
       <div>
-        <Link to="/checkin">Checkin</Link>
+        <Link to="/checkin" className={styles.link}>
+          Checkin
+        </Link>
       </div>
       <div>
-        <Link to="/register">Create a New Account</Link>
+        <Link to="/register" className={styles.link}>
+          Create a New Account
+        </Link>
       </div>
       <div>
-        <Link to="/search">Find An Item</Link>
+        <Link to="/search" className={styles.link}>
+          Find An Item
+        </Link>
       </div>
       <div className={styles.logout}>Logout</div>
     </div>
