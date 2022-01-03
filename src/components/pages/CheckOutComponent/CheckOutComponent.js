43d77d4e4d69673a9d832cd12ab706122a2d6571
stefmryan <stefmryan@gmail.com>
2022-01-02 18:26:21 -0600

    working in checkoutcomponent

--- src/components/pages/CheckOutComponent/CheckOutComponent.js
+++ src/components/pages/CheckOutComponent/CheckOutComponent.js
@@ -1,11 +1,42 @@
 import React from "react";
+import styles from "../CheckOutComponent/CheckOutComponent.module.css";
 
 const CheckOutComponent = () => {
+  // const [accountObj, setAccountObj] = useState({
+  //   libraryAccountNumber: 0,
+  //   email: "",
+  //   handle: "",
+  //   firstName: "",
+  //   lastName: "",
+  //   driverLicenseNumber: "",
+  //   birthdate: "",
+  //   telephone: "",
+  //   street: "",
+  //   street2: "",
+  //   city: "",
+  //   state: "",
+  //   zipCode: "",
+  //   county: {
+  //     id: "",
+  //     label: "",
+  //   },
+  //   accountType: {
+  //     id: "",
+  //     label: "",
+  //   },
+  // });
+
+  const handleLibraryAccount = (e) => {
+    if (e.key === "Enter") {
+      console.log(e.target.value);
+    }
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <label>
         Library Account
-        <input />
+        <input type="number" onKeyDown={handleLibraryAccount} />
       </label>
       <label>
         Item
