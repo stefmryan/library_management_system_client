080e4e347c8be1aec7d408357f1845d8dfc74509
stefmryan <stefmryan@gmail.com>
2022-01-08 16:17:50 -0600

    before I try to install react fontawesome

--- src/components/AccountComponent/AccountComponent.js
+++ src/components/AccountComponent/AccountComponent.js
@@ -1,21 +1,89 @@
 import React from "react";
-// import styles from "../AccountComponent/AccountComponent.module.css";
+import styles from "../AccountComponent/AccountComponent.module.css";
 
 const AccountComponent = () => {
   return (
     <div>
-      <table>
-        <tr>
-          <thead>Library Account Number</thead>
-          <thead>First Name</thead>
-          <thead>Last Name</thead>
-        </tr>
-        <tr>
-          <td>123456</td>
-          <td>Jane</td>
-          <td>Thompson</td>
-        </tr>
-      </table>
+      <form className={styles.gridcontainer}>
+        <div>
+          <label>
+            First Name
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Last Name
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Barcode
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Telephone
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Library Account Type
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Street Address
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Street Address 2
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            City
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            State
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Zip Code
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            ID Number
+            <input />
+          </label>
+        </div>
+        <div>
+          <label>
+            Birth Date
+            <input />
+          </label>
+        </div>
+      </form>
     </div>
   );
 };
